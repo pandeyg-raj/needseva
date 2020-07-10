156da000ab732719d55117e809783d793169fe60
Hima  Patel <himapatel@Himas-MacBook-Pro.local>
2020-07-10 00:31:56 -0400

    Day2

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "hello world"    
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("Register.html")
+
+@app.route("/signin", methods=["POST","GET"])
+def signin():
+    return render_template("signin.html")
